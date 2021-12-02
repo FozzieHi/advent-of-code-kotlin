04a78e43195c067f565450966280483e3b5f956b
george <george@fozzie.dev>
2021-12-02 17:13:12 +0000

    Add Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var lastLine = 0
+        for (line in input) {
+            val lineInt = line.toInt()
+            if (lineInt > lastLine) {
+                count++
+            }
+            lastLine = lineInt
+        }
+        return count - 1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var sum = 0
+        var lastSum = 0
+        val data = mutableListOf<Int>()
+        for (line in input) {
+            val lineInt = line.toInt()
+            sum += lineInt
+            data.add(lineInt)
+            if (data.size == 4) {
+                sum -= data[0]
+                data.removeFirst()
+                if (sum > lastSum) {
+                    count++
+                }
+                lastSum = sum
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
